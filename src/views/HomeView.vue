0f9cbddf0d795cb67b20d11b5f07170f4c0d6ff6
Geoffrey Arner <geoffarner5@gmail.com>
2022-08-23 12:38:42 -0500

    Added Capstone to Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,37 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
+
+  <div class="capstone">
+    <div class="container">
+      <h1>Capstone Name: {{ capstone.name }}</h1>
+      <p>Description: {{ capstone.description }}</p>
+      <p>Capstone URL: {{ capstone.url }}</p>
+      <img src="screenshot" alt="capstone-screenshot" />
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      capstone: [],
+    };
+  },
+  created: function () {
+    this.indexCapstones();
+  },
+  methods: {
+    indexCapstone: function () {
+      axios.get("/capstones").then((response) => {
+        console.log("capstones index", response);
+        this.capstones = response.data;
+      });
+    },
   },
 };
 </script>
