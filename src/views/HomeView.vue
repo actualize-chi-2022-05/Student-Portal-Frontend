5fc960c796c693e7f2604b3a26b346e61b9f278a
ericjbarton <erockbart@gmail.com>
2022-08-23 13:38:19 -0400

    adds create function and method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,38 @@
-<template>
-  <div class="experience">
-    <h1>Experience</h1>
-    <div v-for="experience in experience" v-bind:key="experience.id" >
-    <p>Start Date: {{ experience.startDate }}</p>
-    <p>End Date: {{ experience.endDate }}</p>
-    <p>Job Title: {{ experience.jobTitle }}</p>
-    <p>Company Name: {{ experience.companyName }}</p>
-    <p>Details: {{ experience.details }}</p>
-  </div>
-</template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import { assertExpressionStatement } from "@babel/types";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      experience: [],
+    };
+  },
+  created: function () {
+    this.indexExperience();
+  },
+  methods: {
+    indexExperience: function () {
+      axios.get("/experience").then((response) => {
+        console.log("experience index", response);
+        this.experience = response.data;
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="experience">
+    <h1>Experience</h1>
+    <div v-for="experience in experience" v-bind:key="experience.id">
+      <p>Start Date: {{ experience.startDate }}</p>
+      <p>End Date: {{ experience.endDate }}</p>
+      <p>Job Title: {{ experience.jobTitle }}</p>
+      <p>Company Name: {{ experience.companyName }}</p>
+      <p>Details: {{ experience.details }}</p>
+    </div>
+  </div>
+</template>
